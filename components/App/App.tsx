53474179e00ad62bef2b6353c9e16d587f133af1
SashaOsp <sasha.mykol@gmail.com>
2026-05-15 10:34:47 +0300

    start ref

---
+++ components/App/App.tsx
@@ -0,0 +1,63 @@
+import css from "./App.module.css";
+import NoteList from "../NoteList/NoteList";
+import Pagination from "../Pagination/Pagination";
+import Modal from "../Modal/Modal";
+import NoteForm from "../NoteForm/NoteForm";
+import SearchBox from "../SearchBox/SearchBox";
+
+import { fetchNotes } from "../../lib/api";
+
+import { useState } from "react";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useDebouncedCallback } from "use-debounce";
+
+export default function App() {
+  const [note, setNote] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const queryKey = ["notes", note, currentPage];
+
+  const { data, isLoading } = useQuery({
+    queryKey,
+    queryFn: () => fetchNotes(currentPage, note),
+    placeholderData: keepPreviousData,
+  });
+
+  const totalPages = data?.totalPages ?? 0;
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  const debouncedSearch = useDebouncedCallback((value: string) => {
+    setNote(value);
+    setCurrentPage(1);
+  }, 300);
+
+  const hasPagination = totalPages > 1;
+  const hasNotes = data && !isLoading;
+
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        <SearchBox onSearch={debouncedSearch} />
+        {hasPagination && (
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={setCurrentPage}
+          />
+        )}
+        <button onClick={openModal} className={css.button}>
+          Create note +
+        </button>
+        {isModalOpen && (
+          <Modal onClose={closeModal}>
+            <NoteForm onEnd={closeModal} />
+          </Modal>
+        )}
+      </header>
+      {hasNotes && <NoteList notes={data.notes} />}
+    </div>
+  );
+}
---
+++ lib/api.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+import type { Note, NewNote } from "../types/note";
+
+const NOTEHUB_TOKEN = import.meta.env.VITE_NOTEHUB_TOKEN;
+
+axios.defaults.baseURL = "https://notehub-public.goit.study/api/";
+
+export interface NoteService {
+  notes: Note[];
+  totalPages?: number;
+}
+
+export async function fetchNotes(page: number, search: string) {
+  const response = await axios.get<NoteService>("/notes", {
+    params: {
+      page,
+      perPage: 12,
+      search,
+    },
+    headers: {
+      Authorization: `Bearer ${NOTEHUB_TOKEN}`,
+    },
+  });
+  return response.data;
+}
+
+export async function createNote(newNote: NewNote) {
+  const response = await axios.post<Note>("/notes", newNote, {
+    headers: {
+      Authorization: `Bearer ${NOTEHUB_TOKEN}`,
+    },
+  });
+  return response.data;
+}
+
+export async function deleteNote(noteId: string) {
+  const response = await axios.delete<Note>(`/notes/${noteId}`, {
+    headers: {
+      Authorization: `Bearer ${NOTEHUB_TOKEN}`,
+    },
+  });
+  return response.data;
+}
